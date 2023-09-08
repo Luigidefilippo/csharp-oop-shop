8dc91b063aa4eb89d858d759e3bd0bca1cbe751a
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-08 15:32:09 +0200

    Attributi

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Prodotto
+    {
+        //ATTRIBUTI
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private decimal prezzo; 
+        private decimal iva;
+
+        //CODICE SOLO IN LETTURA 
+        public int Codice => codice;
+
+
+    }
+}
